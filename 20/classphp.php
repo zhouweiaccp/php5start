9dbbabe99a716486c9b6c22ef324711462bc7db2
zhouwei@1 <554961776@qq.com>
2014-10-27 20:51:54 +0800

    date

---
+++ 20/classphp.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class tcons {
+
+    // const  TCONST1=date('Y-m-d h:i').;
+    const TCONST1 = 'ss\r\n';
+
+    //static const  TT=date('Y-m-d h:s').'';
+}
+date_default_timezone_set('PRC');
+ setlocale(LC_ALL,"¡±chs");  
+echo date('Y-m-d H:i:s');
+?>
---
+++ 20/tdatetime.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Ê±Çø
+date_default_timezone_set('PRC');
+ setlocale(LC_ALL,"¡±chs");  
+echo date('Y-m-d H:i:s');
+?>
